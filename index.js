e93503a83d3efcb50a41a3763f549647e657980f
ebrugulec <gulecebrueg@gmail.com>
2020-10-27 02:23:43 +0300

    Add workflow

---
+++ index.js
@@ -0,0 +1,18 @@
+const now = new Date();
+const startDate = new Date(now.getFullYear(), 0, 1);
+const endDate = new Date(now.getFullYear() + 1, 0, 1);
+const progressOfThisYear = (now-startDate) / (endDate-startDate);
+
+function generateProgressBar() {
+    const progressBarCapacity = 50
+    const passedProgressBarIndex = parseInt(progressOfThisYear * progressBarCapacity)
+    const progressBar = Array(progressBarCapacity)
+        .fill('▁')
+        .map((value, index) => index < passedProgressBarIndex ? '█' : value)
+        .join('')
+    return `{ ${progressBar} }`
+}
+
+const readMe = `⏳ Year progress ${generateProgressBar()} ${(progressOfThisYear * 100).toFixed(2)} %`
+
+console.log(readMe)
